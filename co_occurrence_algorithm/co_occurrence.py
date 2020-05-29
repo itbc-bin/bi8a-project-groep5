6a99f30c5ddc9593d41c61deed6bf4f2642a77b5
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-05-29 13:32:56 +0200

    updated co occurrence algorithm to only use phenotype from user input

--- co_occurrence_algorithm/co_occurrence.py
+++ co_occurrence_algorithm/co_occurrence.py
@@ -6,9 +6,6 @@ import mysql.connector
 from nltk.tokenize import sent_tokenize
 
 
-# TODO write docomentation
-# TODO handle bugs
-
 class CoOccurrence:
     """
     A class to calculate the co-occurrence of titles and abstracts of articles.
@@ -17,34 +14,38 @@ class CoOccurrence:
     to calculate all of the possible (41709140) combinations.
     :param data: a list that contains dictionary with the info of an article.
     :param url_id: the url id of the search term.
-    :param title: boolean whether or not to include the title for the
+    :param term: the search term (phenotype) from the user input.
+    :param title: an optional title of the algorithm search, from user input.
+    :param in_title: boolean whether or not to include the title for the
     calculation. Default is true.
     :param in_sentence: boolean whether or not to inculde a sentence of the
     abstract for the calculation. Default is true.
     :param in_abstract: boolean whether or not to inculde the abstract for
     the calculation. Default is true.
-    :param multiple_times_in_abstract: boolean whether or not to inculde if a
+    :param in_multiple_abstracts: boolean whether or not to inculde if a
     combination occurs in multiple abstracts for the calculation. Default is
     true.
     """
 
-    def __init__(self, data, url_id, title=None, notify=False, email='',
+    def __init__(self, data, url_id, term, title=None, in_title=True,
                  in_sentence=True, in_abstract=True,
-                 multiple_times_in_abstract=True):
-        self.__directory = f'co_occurrence_algorithm{os.sep}data_files'
+                 in_multiple_abstracts=True):
+        self.__directory = os.path.join('co_occurrence_algorithm',
+                                        'data_files')
         self.data_list = data
         self.url_id = url_id
+        self.phenotype = [term]
         self.gene_symbols = os.path.join(self.__directory,
                                          'GenPanels_merged_DG-2.17.0.tsv')
-        self.phenotypes = os.path.join(self.__directory,
-                                       'gene_condition_source_id.txt')
         self.combinations = []
         self.title = title
-        self.notify = notify
-        self.email = email
+        self.in_title = in_title
         self.in_sentence = in_sentence
         self.in_abstract = in_abstract
-        self.multiple_times_in_abstract = multiple_times_in_abstract
+        self.in_multiple_abstracts = in_multiple_abstracts
+        self.options = [{'in title': in_title, 'in sentence': in_sentence,
+                         'in abstract': in_abstract,
+                         'multiple times in abstract': in_multiple_abstracts}]
         self.tokenized = []
         self.co_occurrence = {}
         self.__get_combinations()
@@ -62,6 +63,7 @@ class CoOccurrence:
             abstract = self.__pre_process_abstract(data['Abstract']).strip()
             abstract_sentences = sent_tokenize(abstract)
             data_dict['abstract'] = abstract_sentences
+            data_dict['pmid'] = data['PMID']
             self.tokenized.append(data_dict)
 
     def calculate_co_occurrence(self):
@@ -78,30 +80,28 @@ class CoOccurrence:
         different abstracts. A dictionary will be made that keeps track of
         combinations with a score higher than 0.
         """
-        print(len(self.combinations))
         for data in self.tokenized:
+            abstract = ' '.join(data['abstract'])
             for combination in self.combinations:
                 gene_space = f' {combination[0]} '
                 gene_parentheses = f'({combination[0]})'
                 phenotype = combination[1].lower()
-                if self.title:
+                if self.in_title:
                     self.__check_part(gene_space, gene_parentheses,
                                       phenotype, data['title'],
-                                      10)
+                                      10, data['pmid'])
 
                 if self.in_sentence:
                     for sentence in data['abstract']:
                         self.__check_part(gene_space, gene_parentheses,
-                                          phenotype, sentence, 5)
+                                          phenotype, sentence, 5, data['pmid'])
                 if self.in_abstract:
-                    abstract = ' '.join(data['abstract'])
                     self.__check_part(gene_space, gene_parentheses,
-                                      phenotype, abstract, 2)
+                                      phenotype, abstract, 2, data['pmid'])
 
-        if self.multiple_times_in_abstract:
+        if self.in_multiple_abstracts:
             all_abstracts = [' '.join(data['abstract']) for data in
                              self.tokenized]
-
             for combination in self.combinations:
                 amount = 0
                 gene_space = f' {combination[0]} '
@@ -114,27 +114,47 @@ class CoOccurrence:
                         amount += 1
                 if amount > 1:
                     gene = gene_space.strip()
+                    if f'{gene}, {phenotype}' not in self.co_occurrence:
+                        self.co_occurrence[f'{gene}, {phenotype}'] = {}
+                        self.co_occurrence[f'{gene}, {phenotype}'][
+                            'options'] = self.options
                     try:
-                        self.co_occurrence[f'{gene}, {phenotype}'] += 1
+                        self.co_occurrence[f'{gene}, {phenotype}'][
+                            'amount'] += 1
                     except KeyError:
-                        self.co_occurrence[f'{gene}, {phenotype}'] = 1
+                        self.co_occurrence[f'{gene}, {phenotype}'][
+                            'amount'] = 1
 
     def save_to_db(self):
+        """
+        Save co occurrence results to a database, so the results can always
+        be retrieved.
+        """
+        print(self.co_occurrence)
         connection = self.__connection_database()
         cursor = connection.cursor()
+        cursor.execute("select count(*) from algorithm_results")
+        result_id = cursor.fetchone()[0] + 1
         cursor.execute(
-            "insert into results (url_id) values ('{}')".format(self.url_id))
+            "insert into results (url_id, title) values ('{}', '{}')".format(
+                self.url_id, self.title))
         connection.commit()
+
         cursor.execute(
             "select id from results where url_id = '{}'".format(self.url_id))
-        _id = cursor.fetchone()[0]
-        for combi, amount in self.co_occurrence.items():
-            cursor.execute(
-                "insert into algorithm_results "
-                "(results_id, combination, amount) "
-                "values ('{}', '{}', '{}')".format(_id, combi, amount))
+        url_id_db = cursor.fetchone()[0]
+        for key, value in self.co_occurrence.items():
+            cursor.execute("insert into algorithm_results "
+                           "(results_id, combination, amount) values ('{}', "
+                           "'{}', '{}')".format(url_id_db, key,
+                                                value['amount']))
             connection.commit()
-
+            for pmid in value['ids']:
+                cursor.execute(
+                    "insert into pmid_result (algorithm_results_id, "
+                    "pmid) values ('{}', '{}')".format(result_id, pmid))
+                connection.commit()
+            result_id += 1
         connection.close()
 
     def get_co_occurence(self):
@@ -162,7 +182,7 @@ class CoOccurrence:
         return abstract_data
 
     def __check_part(self, gene_space, gene_parentheses, phenotype,
-                     part, amount):
+                     part, amount, pmid):
         """
         Checks is either one of the gene symbol types and the phenotype is
         present in a part of the article, which can be the title, abstract or
@@ -182,10 +202,19 @@ class CoOccurrence:
             or gene_parentheses in part) \
                 and phenotype in part:
             gene = gene_space.strip()
+            if f'{gene}, {phenotype}' not in self.co_occurrence:
+                self.co_occurrence[f'{gene}, {phenotype}'] = {}
+                self.co_occurrence[f'{gene}, {phenotype}'][
+                    'options'] = self.options
             try:
-                self.co_occurrence[f'{gene}, {phenotype}'] += amount
+                self.co_occurrence[f'{gene}, {phenotype}']['amount'] += amount
+                if pmid not in self.co_occurrence[f'{gene}, {phenotype}'][
+                    'ids']:
+                    self.co_occurrence[f'{gene}, {phenotype}']['ids'].append(
+                        pmid)
             except KeyError:
-                self.co_occurrence[f'{gene}, {phenotype}'] = amount
+                self.co_occurrence[f'{gene}, {phenotype}']['amount'] = amount
+                self.co_occurrence[f'{gene}, {phenotype}']['ids'] = [pmid]
 
     def __get_combinations(self):
         """
@@ -195,24 +224,13 @@ class CoOccurrence:
         a new list.
         """
         genes = []
-        phenotypes = []
         with open(self.gene_symbols, 'r') as genes_file:
             genes_file.readline()
             for line in genes_file:
                 gene = line.split('\t')[0]
                 genes.append(gene.strip())
-        with open(self.phenotypes, 'r') as phenotype_file:
-            phenotype_file.readline()
-            for line in phenotype_file:
-                phenotype = line.split('\t')[4]
-                phenotypes.append(phenotype.strip())
 
-        self.combinations = list(itertools.product(genes, phenotypes))
-
-    # TODO send mail with output
-    def send_mail(self):
-        if self.notify and self.email:
-            pass
+        self.combinations = list(itertools.product(genes, self.phenotype))
 
     @staticmethod
     def __connection_database():
