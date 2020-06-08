f4ef105da21e73fe424228a945da48bdd85d47e4
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-06-08 11:18:04 +0200

    added comments, removed unused code

--- static/index.js
+++ static/index.js
@@ -2,14 +2,8 @@ $(document).ready(function () {
     // dropdown functionality
     $('.label.ui.dropdown').dropdown();
 
+    // accordion functionality
     $('.ui.accordion').accordion();
-
-    $('.trigger.example .accordion')
-        .accordion({
-            selector: {
-                trigger: '.title .icon'
-            }
-        });
 
     // table sortable
     $('.table.ui.sortable').tablesort();
@@ -59,6 +53,7 @@ $(document).ready(function () {
     });
 });
 
+// calendar functionality
 $(function () {
     $('#rangestart').calendar({type: 'date'});
 });
@@ -71,8 +66,6 @@ $(function () {
 	https://github.com/kylefox/jquery-tablesort
 	Version 0.0.11
 */
-
-
 (function ($) {
     $.tablesort = function ($table, settings) {
         const self = this;
@@ -205,7 +198,7 @@ $(function () {
 
 })(window.Zepto || window.jQuery);
 
-
+// ajax request to upload new file
 $(function () {
     $('#upload-file-button').click(() => {
         const formData = new FormData($('#upload-form')[0]);
@@ -225,49 +218,3 @@ $(function () {
         })
     });
 });
-//Get the button:
-
-
-//
-// sort function written by Yaris
-//
-//
-// const tableHeaders = document.querySelectorAll('.content-table th')
-// tableHeaders.forEach(headerCell => {
-//     headerCell.addEventListener('click', () => {
-//         const tableElement = headerCell.parentElement.parentElement.parentElement;
-//         const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-//         const currentIsAscending = headerCell.classList.contains('th-sort-asc');
-//
-//         sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
-//     });
-// });
-//
-//
-// function sortTableByColumn(table, column, asc = true) {
-//     const dirModifier = asc ? 1 : -1;
-//     const tBody = table.tBodies[0];
-//     const rows = Array.from(tBody.querySelectorAll('tr'));
-//
-//     // Sort each row
-//     const sortedRows = rows.sort((a, b) => {
-//         const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-//         const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-//
-//         return aColText > bColText ? (dirModifier) : (-1 * dirModifier);
-//     });
-//
-//     // Remove all existing TRs from the table
-//     while (tBody.firstChild) {
-//         tBody.removeChild(tBody.firstChild);
-//     }
-//
-//     // Re-add the newly sorted rows
-//     tBody.append(...sortedRows);
-//
-//     // Remember how the column is currently sorted
-//     table.querySelectorAll('th').forEach(th => th.classList.remove('th-sort-asc', 'th-sort-desc'));
-//     table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-asc', asc);
-//     table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-desc', !asc);
-// }
-
